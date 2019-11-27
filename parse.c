8c6ca1635f6846b666fcb850df7cca6fb4e54cb7
DarthChurros <tnarayan00@stuy.edu>
2019-11-27 07:33:31 -0500

    I think this works

--- parse.c
+++ parse.c
@@ -6,10 +6,27 @@
 char** parse_args(char* line);
 
 int main(int argc, char** argv) {
-  char** args = parse_args(argv[0]);
+  if (argc == 1) {
+    printf("Enter a command!\n\n");
+    return 0;
+  }
+  char** args = parse_args(argv[1]);
   execvp(args[0], args);
+  free(args);
+  return 0;
 }
 
 char** parse_args(char* line) {
-  return NULL;
+  char** split = calloc(6, sizeof(char*));
+  split[5] = NULL;
+  char* arg = line;
+  int i = 0;
+
+  while (arg) {
+    strsep(&arg, " ");
+    split[i] = line;
+    line = arg;
+    i++;
+  }
+  return split;
 }
